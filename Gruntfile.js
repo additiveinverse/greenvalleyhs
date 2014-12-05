7b880e24c0187195fa81e7979c4375eaa354802d
John Costanzo <jrock2004@gmail.com>
2014-12-05 12:33:36 -0500

    Working on Grunt File

--- Gruntfile.js
+++ Gruntfile.js
@@ -45,11 +45,122 @@ module.exports = function(grunt) {
 				},
 				files: manifest
 			}
+		},
+		copy: {
+			less: {
+				expand: true,
+				cwd: '<%= bower %>',
+				src: ['normalize-less/*.less'],
+				dest: '<%= app.less %>',
+				flatten: true
+			},
+			sourcemaps: {
+				expand: true,
+				cwd: '<%= bower %>',
+				src: [
+					'angular-*/*.map',
+					'jquery/dist/*.map'
+				],
+				dest: '<%= prod.js %>',
+				flatten: true
+			},
+			jsmodules: {
+				expand: true,
+				cwd: '<%= bower %>',
+				src: [
+					'angular/angular.min*',
+					'angular-route/angular-route.js',
+					'jquery/dist/*.min.js',
+					'angular-sanitize/*.js'
+				],
+				dest: '<%= prod.js %>',
+				flatten: true
+			},
+			jsFiles: {
+				expand: true,
+				cwd: '<%= app.js %>',
+				src: [ '*.js' ],
+				dest: '<%= prod.js %>',
+				flatten: true
+			},
+			libFiles: {
+				expand: true,
+				cwd: '<%= app.lib %>',
+				src: [ '*' ],
+				dest: '<%= prod.lib %>',
+				flatten: true
+			},
+			extFiles: {
+				expand: true,
+				cwd: '<%= app.files %>',
+				src: [ '*' ],
+				dest: '<%= prod.files %>',
+				flatten: true
+			},
+			angular: {
+				expand: true,
+				cwd: '<%= app.root %>',
+				src: [ 'app.js' ],
+				dest: '<%= prod.js %>',
+				flatten: true
+			},
+			mainView: {
+				expand: true,
+			        cwd: '<%= app.root %>',
+			        src: [ 'index.html' ],
+			        dest: '<%= prod.root %>',
+			        flatten: true
+			},
+			angularViews: {
+				expand: true,
+				cwd: '<%= app.views %>',
+				src: [ '**/*.html' ],
+				dest: '<%= prod.views %>',
+				flatten: true
+			},
+			img: {
+				expand: true,
+				cwd: '<%= app.img %>',
+				src: [ '**/*' ],
+				dest: '<%= prod.img%>',
+				flatten: false
+			}
+		},
+		connect: {
+			server: {
+				options: {
+					port: '9001',
+					base: 'build/',
+					protocol: 'http',
+					livereload: true,
+					open: {
+						target: 'http://localhost:9001',
+						callback: function() {}
+					}
+				}
+			}
+		},
+		watch: {
+			files: [
+				'<%= app.root %>**/*',
+        			'Gruntfile.js'
+			],
+			tasks: [
+				'copy',
+				'less:dev'
+			],
+			options: {
+				reload: false,
+				livereload: true,
+				spawn: true
+			}
 		}
 	});
 
 	require('matchdep').filterDev('grunt-*').forEach( grunt.loadNpmTasks );
 
 	// The following are the grunt tasks that you can run
-	grunt.registerTask('default', [ '' ]);
+	grunt.registerTask('default', [ 'less:dev', 'connect', 'watch' ]);
+	grunt.registerTask('first', [ 'copy', 'less:dev' ]);
+	grunt.registerTask('deploy', [ 'copy', 'less:prod' ]);
 };
